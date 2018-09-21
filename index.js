00413d12f6b87b20d9982b96e9ce35ce3cd852b2
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-21 10:40:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-function Point(x, y) {
-  this.x = x
-  this.y = y
-}
+function Point(x, y){
+  this.x = x;
+  this.y = y;
+};
 
 function Side(length) {
-  this.length = length
-}
+  this.length = length;
+};
 
-Point.prototype.toString = function() {
+Point.prototype.toString = function(){
   var a = this.x.toString();
   var b = this.y.toString();
   return (/a,\s?b/);
-}
+};
 
+function Shape() {
+  this.position = null;
+};
 
-shape.prototype = Object.create(shape.prototype)
-shape.prototype.constructor = Shape;
-
-shape.prototype.addToPlane = function(x, y) {
-  this.position = new Point (x, y)
+Shape.prototype = Object.create(Shape.prototype);
+Shape.prototype.constructor = Shape;
+Shape.prototype.addToPlane = function(x, y) {
+  this.position = new Point(x, y);
 }
 
 Shape.prototype.move = function(x, y) {
   this.addToPlane(x, y);
 };
+
+
 
 function Circle(radius) {
   this.radius = radius;
