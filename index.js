5ddaa4eaf0829140df428f7f7edffee6cfd7cf96
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-09-21 10:39:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ function Point(x, y) {
 }
 
 function Side(length) {
-  this.length = length 
+  this.length = length
 }
 
 Point.prototype.toString = function() {
-  var a = this.x.toString(); 
+  var a = this.x.toString();
   var b = this.y.toString();
   return (/a,\s?b/);
+}
+
+
+shape.prototype = Object.create(shape.prototype)
+shape.prototype.constructor = Shape; 
+shape.prototype.addToPlane = function(x, y) {
+  this.position = new Point (x, y);
 }
